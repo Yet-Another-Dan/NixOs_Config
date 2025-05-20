bc2d9b75aa42e1c9e20862555fa0eaa60b5d9809
Yet-Another-Dan <dmcevoy@tutamail.com>
2025-05-19 22:58:50 -0400

    create baseline apps modules

--- modules/base.nix
+++ modules/base.nix
@@ -13,7 +13,7 @@ in {
     };
   };
 
-  config = mkIf cfg.enable = {
+  config = mkIf cfg.enable {
     programs.firefox.enable = true;
     programs.fish.enable = true;
     services.flatpak.enable = true;
