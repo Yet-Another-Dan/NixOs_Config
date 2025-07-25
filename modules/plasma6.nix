66574581dbbda1432d61a38199ec71889878b11b
Yet-Another-Dan <dmcevoy@tutamail.com>
2025-07-24 22:42:14 -0400

    theme sddm using sddm-astronaut-theme

--- modules/plasma6.nix
+++ modules/plasma6.nix
@@ -18,15 +18,32 @@ in {
     services.displayManager.sddm.enable = true;
     services.displayManager.defaultSession = "plasma";
     services.displayManager.sddm.wayland.enable = true;
+    services.displayManager.sddm.theme = "sddm-astronaut-theme";
     services.desktopManager.plasma6.enable = true;
 
     programs.dconf.enable = true;
     environment.systemPackages = with pkgs; [
+      qt6.qtmultimedia
 	    kdePackages.okular
 	    kdePackages.plasma-browser-integration
+
+      (pkgs.stdenv.mkDerivation {
+        name = "sddm-astronaut-theme";
+        src = pkgs.fetchFromGitHub {
+          owner = "Keyitdev";
+          repo = "sddm-astronaut-theme";
+          rev = "master";
+          sha256 = "sha256-33CzZ4vK1dicVzICbudk8gSRC/MExG+WnrE9wIWET14=";
+        };
+        installPhase = ''
+          mkdir -p $out/share/sddm/themes
+          cp -R $src $out/share/sddm/themes/sddm-astronaut-theme
+        '';
+      })
     ] ++ cfg.extraPackages;
 
     programs.kdeconnect.enable = true;
+
   };
 
 
